04d757035a2306cdee423108a05ff4ecac0eb3da
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-04-09 19:07:30 +0530

    Update leap_year.cpp

---
+++ cpp-basic-programs/conditions/leap_year.cpp
@@ -0,0 +1,13 @@
+// Question: Write a C++ program to check leap year.
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int year;
+    cin>>year;
+    if((year%4==0 && year%100!=0) || year%400==0)
+        cout<<"Leap Year";
+    else
+        cout<<"Not Leap Year";
+}
