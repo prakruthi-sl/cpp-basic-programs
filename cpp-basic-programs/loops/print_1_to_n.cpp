be41b142ee79b0c3d0d929bb0062f2062a52e00a
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-04-11 20:59:55 +0530

    Update print_1_to_n.cpp

---
+++ cpp-basic-programs/loops/print_1_to_n.cpp
@@ -0,0 +1,11 @@
+// Question: Write a C++ program to print numbers from 1 to N.
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+        cout<<i<<" ";
+}
