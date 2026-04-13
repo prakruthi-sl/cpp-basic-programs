5f65085e3c350a4d24e2caf435dd84e85c39885b
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-04-13 07:26:55 +0530

    Update factorial.cpp

---
+++ cpp-basic-programs/loops/factorial.cpp
@@ -0,0 +1,13 @@
+// Question: Write a C++ program to find factorial.
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int n;
+    long long fact=1;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+        fact*=i;
+    cout<<"Factorial = "<<fact;
+}
