cd65bd43c02a73f546c9d1bc31687c02b0239980
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-04-16 17:27:17 +0530

    Update prime_number.cpp

--- cpp-basic-programs/number_logic/prime_number.cpp
+++ cpp-basic-programs/number_logic/prime_number.cpp
@@ -2,15 +2,13 @@
 
 #include <iostream>
 using namespace std;
-
-int main() {
+int main() 
+{
     int n,count=0;
     cin>>n;
-
     for(int i=1;i<=n;i++)
         if(n%i==0)
             count++;
-
     if(count==2)
         cout<<"Prime";
     else
