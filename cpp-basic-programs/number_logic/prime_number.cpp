131398be9ec9bd6790d2191b563deea2d5936eea
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-04-16 17:26:46 +0530

    Update prime_number.cpp

---
+++ cpp-basic-programs/number_logic/prime_number.cpp
@@ -0,0 +1,18 @@
+// Question: Write a C++ program to check prime number.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,count=0;
+    cin>>n;
+
+    for(int i=1;i<=n;i++)
+        if(n%i==0)
+            count++;
+
+    if(count==2)
+        cout<<"Prime";
+    else
+        cout<<"Not Prime";
+}
